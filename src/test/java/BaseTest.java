cab6a70c0dbbcfc921056b855fd966ad495badc1
Yokesh <yokeshwararao@hotmail.com>
2025-09-23 23:44:46 +0530

    first commit

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,17 +3,19 @@ package base;
 import org.openqa.selenium.WebDriver;
 
 import Constants.FrameworkConstants;
+//import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
     protected WebDriver driver;
 
-    @BeforeMethod
+    @BeforeMethod(alwaysRun = true)
     public void setUp() {
         driver = DriverFactory.initDriver(FrameworkConstants.CHROME);
         driver.get(FrameworkConstants.SAUCEDEMO_URL);
     }
 
-    @AfterMethod
+//    @AfterMethod
     public void tearDown() {
         DriverFactory.quitDriver();
     }
